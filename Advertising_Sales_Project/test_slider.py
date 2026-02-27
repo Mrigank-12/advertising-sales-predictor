5416cf68422679ec05ea056d5d5ac942ef6a5ee1
Mrigank <mrigank.amitp@gmail.com>
2026-02-27 16:16:47 +0530

    Delete Advertising_Sales_Project directory

--- Advertising_Sales_Project/test_slider.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.sidebar.slider('plain int', 0, 100000, 15000, step=5000)
-st.sidebar.slider('format str %d', 0, 100000, 15000, step=5000, format="%d")
-st.sidebar.slider('format localized', 0, 100000, 15000, step=5000, format="localized")
-st.sidebar.slider('format commas', 0, 100000, 15000, step=5000, format="%d")
